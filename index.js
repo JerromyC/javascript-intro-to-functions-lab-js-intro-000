8c8b22d01ef466f4b6a93f66739153e4683e56e2
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-20 21:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {return "I can\'t hear you!";}
+  if (string.toLowerCase()) {return "I can\'t hear you!";
+} else if (string.toUpperCase()) {return "YES INDEED!"}
 }
 
-function sayHiToGrandma(string) {
-  if (string.toUpperCase()) {return "YES INDEED!";}
-}
+
